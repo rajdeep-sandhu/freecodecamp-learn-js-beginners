4d6599b936a3f4a935b25ea4e6e7aa557af9d8bc
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-10-04 13:56:48 +0100

    add capitalizeFirst and variable exports

--- string_function.js
+++ string_function.js
@@ -1,3 +1,12 @@
 // string function for import and export example
 
 export const capitalizeString = str => str.toUpperCase()
+
+const capitalizeFirst = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export { capitalizeFirst };
+
+export const foo = "foo";
+export const bar = "bar";
